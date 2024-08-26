8b02d51ad5e3a729ab9743471e76acf5e728b409
Fabio Lopes <nunes-lopes@hotmail.com>
2024-08-25 23:40:23 -0300

    operadores maior menor igual

---
+++ 5_op_e_expressoes/19_maior_ou_igual/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $a = 4;
+    $b = 5;
+    $c = 6;
+    $d = 6;
+
+    if( $a > $b) {
+        echo "A é maior que B <br>";
+    }
+
+    if( $a < $b) {
+        echo "B é maior que A <br>";
+    }
---
+++ 5_op_e_expressoes/20_menor_ou_igual/index.php
@@ -0,0 +1,5 @@
+<?php
+
+    if(4 < 12) {
+        echo "4 menor que 12 <br>";
+    }
