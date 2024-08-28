194f0c817fa8d5895d27150dc6ca06d758e7555e
Fabio Lopes <nunes-lopes@hotmail.com>
2024-08-27 23:20:20 -0300

    estrutura de repetição while

---
+++ 7_estruturas_de_repeticao/1_while/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+    $x = 0; // definição do contador
+
+    while($x < 10) { // inicio da definicição da estrutura
+        
+        echo $x . "<br>"; // corpo do loop
+
+        $x++; //incremento do contador
+    }
+
+    echo "Continua <br> <br>";
+
+
+    $y = 0; // definição do contador
+
+    while($y <= 10) { // inicio da definicição da estrutura
+        
+        echo $y . "<br>"; // corpo do loop
+
+        $y += 2; // $y = $y + 2; //incremento do contador
+    }
+
+    echo "Continua <br> <br>";
+
+    $z = 10; // definição do contador
+
+    while($z > 0) { // inicio da definicição da estrutura
+        
+        echo $z . "<br>"; // corpo do loop
+
+        $z--; // $y = $y - 1; //incremento do contador
+    }
+
+    echo "Continua <br> <br>";
+
+   
+    $a = 10; // definição do contador
+
+    while($a > 0) { // inicio da definicição da estrutura
+        
+        if($a % 2 != 0)
+        echo $a . "<br>"; // corpo do loop
+
+        $a--; // $y = $y - 1; //incremento do contador
+    }
+
+    echo "Continua <br> <br>";
