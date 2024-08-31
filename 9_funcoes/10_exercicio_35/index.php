20a53cbb1440481360943dceb00a682e694c3a33
Fabio Lopes <nunes-lopes@hotmail.com>
2024-08-31 18:42:12 -0300

    exercicio escopo

---
+++ 9_funcoes/10_exercicio_35/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+    function numero($n) {
+
+       return $n ** 10 . "<br>"; // numero ao quadrado
+    }
+
+   echo numero(5);
+   echo numero(2);
+   echo numero(3);
---
+++ 9_funcoes/11_escopo/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$a = 10;
+$b = 15;
+
+function testeEscopo() {
+    
+    $a = 5;
+
+    global $b;
+    static $c = 0;
+
+    $a++;
+
+    $b++;
+
+    $c++;
+
+    echo "ESCOPO LOCAL DE A:  $a <br>";
+    echo "ESCOPO GLOBAL NA FUNÇÃO DE  B: $b <br>";
+    echo "ESCOPO STATIC DE C: $c <br>";
+
+}
+
+echo "ESCOPO GLOBAL DE A: $a <br>";
+echo "ESCOPO GLOBAL DE B: $b <br>";
+
+testeEscopo();
+
+testeEscopo();
