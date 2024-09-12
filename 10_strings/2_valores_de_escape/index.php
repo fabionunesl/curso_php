376f236a892c854fc8cd7204748ba150d1613d7a
Fabio Lopes <nunes-lopes@hotmail.com>
2024-09-12 11:27:12 -0300

    Introdução a strings

---
+++ 10_strings/2_valores_de_escape/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    header("Content-type: text/plain"); // comando para dizer que é texto puro e não html, serve para entender comandos como "\n"
+    
+    
+    
+    // pular linha
+    echo  "Isso vai ficar na primeira linha \n E isso na segunda linha \n";
+
+
+    // Tab
+
+    echo "Testando o tab \t \t \t aqui \n";
+
+    // Barra invertida
+
+    echo "Barra invertida \\ \n";
+
+    // Dólar
+
+    echo "imprimindo o dólar \$teste";
+
