3652e795ec1d55b6b9f5c2ed99ee0c1ebc85bc44
Fabio Lopes <nunes-lopes@hotmail.com>
2024-09-13 11:53:06 -0300

    função print e printf

---
+++ 10_strings/4_funcao_print/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    print("imprimindo algo com o print <br>");
+
+
+    $a = "Fábio <br>";
+
+    print ($a);
+
+    $carro = "Fiat";
+    $valor = 2000;
+
+    print("O carro da marca $carro é do valor $valor <br>");
---
+++ 10_strings/5_funcao_printf/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nome = "Fábio";
+
+
+    // %s -> string
+    printf ("O nome é %s <br>", $nome);
+
+    
+
+    // %d -> int  eiros
+
+    $n = 10;
+
+    printf("O número é %d e o outro é %d, %d %s <br>", $n, 150, 200, "carol");
+
+    // %f -> float
+
+    printf("A temperatura atual é de %.1f,    %.2f <br>", 12.456, 23.456677);
