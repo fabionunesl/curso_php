b97c445bfc4cc4cb52395f3dc77fd27d2d3cf881
Fabio Lopes <nunes-lopes@hotmail.com>
2024-09-13 11:33:06 -0300

    exercicio itens mais caros

--- 10_strings/3_exercicio_39/index.php
+++ 10_strings/3_exercicio_39/index.php
@@ -1,2 +1,28 @@
 <?php
 
+$arr = [
+    "carro" => 50000,
+    "sofá" => 1500,
+    "cafeteira" => 200,
+    "televisor" => 2500
+];
+
+function intemCaro($arr) {
+
+    $arrCaros = [];
+
+   foreach($arr as $item => $preco) {
+
+    if($preco > 2000) {
+
+        array_push($arrCaros, $item);
+    }
+   }
+
+   return $arrCaros;
+
+}
+
+$novoArr = intemCaro($arr);
+
+print_r($novoArr);
