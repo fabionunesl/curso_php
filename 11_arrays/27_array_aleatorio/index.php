2160c3688853eaa5b4c6837ce74c97a2f1d8f23a
Fabio Lopes <nunes-lopes@hotmail.com>
2025-04-22 18:58:18 -0300

    array reverse e aleatorio

---
+++ 11_arrays/27_array_aleatorio/index.php
@@ -0,0 +1,19 @@
+<?php
+
+  $arr = range(1, 20);
+
+  
+  print_r($arr);
+  echo "<br>";
+  echo "<br>";
+
+  shuffle($arr);
+
+  print_r($arr);
+  echo "<br>";
+  echo "<br>";
+
+  shuffle($arr);
+
+  print_r($arr);
+  echo "<br>";
